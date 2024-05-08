5d44f8dffc92fb755f80369f116634e4a401866b
bbzwadmin <bbzwadmin@W11-WORK23>
2024-05-08 11:12:30 +0200

    Projektdateien hinzufügen.

---
+++ Finance_Lord/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Finance_Lord
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    
+    public MainWindow()
+    {
+      InitializeComponent();
+      
+    }
+    
+    
+
+    private void Btn_x_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void BtnImbclick(object sender, RoutedEventArgs e)
+    {
+      WindowImmobilien ObjwindowImmobilien= new WindowImmobilien();
+      this.Visibility = Visibility.Hidden;
+      ObjwindowImmobilien.Show();
+    }
+
+    private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+    {
+
+        }
+    }
+}
---
+++ Finance_Lord/WindowImmobilien.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Finance_Lord
+{
+    /// <summary>
+    /// Interaktionslogik für WindowImmobilien.xaml
+    /// </summary>
+    public partial class WindowImmobilien : Window
+    {
+    static int anzahl = 0;
+    static double value1zimmerplus;
+    static int value1zimmer;
+    static int budget;
+
+    public WindowImmobilien()
+        {
+            InitializeComponent();
+            updatecount();
+        }
+
+    private void BtnBack(object sender, RoutedEventArgs e)
+    {
+      MainWindow ObjMainWindow = new MainWindow();
+      this.Visibility = Visibility.Hidden;
+      ObjMainWindow.Show();
+    }
+
+    private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+    {
+
+    }
+
+    private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+    {
+
+    }
+
+    private void TextBox_TextChanged_2(object sender, TextChangedEventArgs e)
+    {
+
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+      if (budget>=value1zimmer){
+        anzahl += 1;
+        budget -= value1zimmer;
+        value1zimmerplus = value1zimmer * 1.2;
+        value1zimmer = Convert.ToInt32(value1zimmerplus);
+        updatecount();
+      }
+    }
+    private void updatecount(){
+      string anzahlzimmer1 = "Anzahl: " + anzahl;
+      count.Text = anzahlzimmer1;
+    }
+  }
+}
